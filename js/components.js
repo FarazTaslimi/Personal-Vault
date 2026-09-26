904f1b72c996fd35346379b9491f868e26e2d305
Faraz Taslimi <Faraz.Taslimi@gmail.com>
2026-09-26 20:47:41 +0330

    V1 (Commented)

--- js/components.js
+++ js/components.js
@@ -1,3 +1,3 @@
 export { mini_card } from '../components/mini-card.js';
 export { song_card } from '../components/song-card.js';
-export { player } from '../components/player.js';
+export { player } from '../components/player.js'; 
